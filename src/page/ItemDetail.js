10aa2bd281241791bb9f89fed4df4f241bfa101c
PARK JEE HO <n2201121@gmail.com>
2020-07-20 18:58:18 +0900

    change ItemDetail page

--- src/page/ItemDetail.js
+++ src/page/ItemDetail.js
@@ -28,19 +28,21 @@ class ItemDetail extends React.Component{
                 </div>
                 <div className="itemDetail">
                     <div className="itemDetail_item_info">
-                        <h2>ITEM Info</h2>
-                        <br/>
+                        {/* <h2>ITEM Info</h2> */}
+                        {/* <br/> */}
                         <br/>
                         <h5>BRAND NAME</h5>
                         <h3>ITEM NAME</h3>
                         
                         <div className="itemDetail_img">
-                            <img src="https://image2.shilladfs.com/files/product/2016/05/04/084613000059_20160504_12400075_13_379150_20141015_183929_org_L32P.jpg" alt="item image"
+                            <img src="https://image2.shilladfs.com/files/product/2016/05/04/084613000059_20160504_12400075_13_379150_20141015_183929_org_L32P.jpg" 
+                            alt="item_image"
                             width={400} />
                         </div>
                         <br/>
                         <br/>
-                        <h3>PRICE: 50,000₩   </h3>
+                        <h3> PRICE: 50,000₩   </h3>
+                        <br/>
                         <div className="itemDetail_discount_info">
                             <Link to="discount_info">
                                 <h3>DISCOUNT INFO</h3>
