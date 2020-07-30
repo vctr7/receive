4767ab057ce78bae5039a96e644b3dabe4f5ab39
PARK JEE HO <n2201121@gmail.com>
2020-07-30 20:52:13 +0900

    substitue Express to Koa

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const Router = require('koa-router');
+
+const router = new Router();
+const app = new Koa();
+
 const port = process.env.PORT || 8795;
 
-// const rjson = require('./parfum.json');
 const data = fs.readFileSync('./database.json');
 const conf = JSON.parse(data);
 const mysql = require('mysql2');
@@ -18,16 +21,21 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));                                                                  
+app.use(bodyParser());
 
-app.get('/api/parfum', (req, res) => {
-    connection.query(
-        "SELECT * FROM parfum WHERE imgSrc IS NOT NULL LIMIT 200",
-        (err, rows, fields) => {
-            res.send(rows);
-        }
-    );
-});
+router.get('/api/parfum', ctx => {
+    try {
+      return new Promise(function(resolve, reject) {
+        connection.query("SELECT * FROM parfum WHERE imgSrc IS NOT NULL LIMIT 200",function (error, results, fields){
+              ctx.body = results;
+              resolve();
+          })
+      });
+    } catch (error) {
+      console.log(error)
+    }
+})
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
