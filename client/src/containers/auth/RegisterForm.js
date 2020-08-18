d2d4033ad52e5e1b1fa93196fcdd3871a36dcba3
PARK JEE HO <n2201121@gmail.com>
2020-08-18 10:36:41 +0900

    Add maintaining login status

--- client/src/containers/auth/RegisterForm.js
+++ client/src/containers/auth/RegisterForm.js
@@ -71,9 +71,12 @@ const RegisterForm = ( { history }) => {
     // user 값이 잘 설정되었는지 확인
     useEffect(() => {
         if (user) {
-            console.log('check API 성공');
-            console.log(user);
             history.push('/');
+            try {
+                localStorage.setItem('user', JSON.stringify(user));
+            } catch(e) {
+                console.log('localStroage is not working..');
+            }
         }
     }, [history, user]);
 
--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer, { rootSaga } from './modules';
+import { tempSetUser, check } from './modules/user';
 
 import 'react-slideshow-image/dist/styles.css'
 import "./scss/style.scss";
@@ -19,8 +20,20 @@ const store = createStore(
   composeWithDevTools(applyMiddleware(sagaMiddleware))
 );
 
-sagaMiddleware.run(rootSaga);
+function loadUser() {
+  try{
+    const user = localStorage.getItem('user');
+    if (!user) return; //로그인 상태 아닐 시 아무것도 안함
 
+    store.dispatch(tempSetUser(user));
+    store.dispatch(check());
+  } catch(e) {
+    console.log('localStorage is not working..');
+  }
+};
+
+sagaMiddleware.run(rootSaga);
+loadUser();
 
 ReactDOM.render(
       <Provider store={store}>
